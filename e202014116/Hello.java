48edbb336af12a25c822398d19e86aa863d39d8c
Yoon, Chanhyeok <jomecena26@naver.com>
2024-09-25 09:51:04 +0900

    .

--- e202014116/Hello.java
+++
@@ -1,7 +0,0 @@
-package e202014116;
-
-public class Hello {
-	public static void run() {
-	System.out.println("hello world");
-}
-}
---
+++ src/e202014116/Main.java
@@ -0,0 +1,8 @@
+package e202014116;
+
+
+public class Main {
+public static void main(String[]args) {
+	System.out.println("윤찬혁");
+}
+}
