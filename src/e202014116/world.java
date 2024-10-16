698eac965d8983fc8aa589209b6f06ad3e69a898
Yoon, Chanhyeok <jomecena26@naver.com>
2024-10-16 09:44:09 +0900

    three

--- src/e202014116/world.java
+++ src/e202014116/world.java
@@ -1,8 +1,6 @@
 public class World {
 public static void run() {
-System.out.println("201514099");
-}
-public static void run2() {
-System.out.println("hello world");
+System.out.println("202014116");
+
 }
 }
